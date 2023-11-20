bb0c4728b53c086c98b500d08057484c6a39fca3
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-20 11:03:29 +0200

    uzd

--- js/practice.js
+++ js/practice.js
@@ -1,15 +1,34 @@
 'use strict';
 console.log('practice.js file was loaded');
 
-const mixed = ['red', true, 5, 7, 'white', 10];
+const mixed = ['red', true, 5, 7, 'white', 10, NaN, null];
 
 // for i
+
+function makeForI() {
+  for (let i = 0; i < mixed.length; i++) {
+    const element = mixed[i];
+    console.log(`typeof ${element} ===`, typeof element);
+  }
+}
+// makeForI();
 // for of
+const makeForOf = function () {
+  for (let element of mixed) {
+    console.log(`typeof ${element} ===`, typeof element);
+  }
+};
+// makeForOf();
+
 // forEach
+const stringsArr = [];
+mixed.forEach((element) => {
+  console.log(`typeof ${element} ===`, typeof element);
+});
 
 // atspausdinti mixed masyvo tipus
-console.log(typeof mixed[0]);
-console.log(typeof mixed[1]);
+// console.log(typeof mixed[0]);
+// console.log(typeof mixed[1]);
 
 // i nauja masyva sudeti tik skaicius
 
