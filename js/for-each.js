5f740d31454388816261ea1e006bd46a69f234a6
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-20 10:42:18 +0200

    types

--- js/for-each.js
+++ js/for-each.js
@@ -9,7 +9,7 @@ for (let spalva of colors) {
 
 // arr.forEach((el, index, arr) => {function body})
 const colUpper = [];
-colors.forEach(function (spalva, idx, arr) {
+colors.forEach((spalva, idx, arr) => {
   console.log(`index: ${idx}`, spalva);
   // galiu keisti esama masyva
   // colors[idx] = spalva + ' spalva';
@@ -21,9 +21,27 @@ console.log('colors ===', colors);
 const nums = [5, -7, 1, -8, 7, 15, -8, 2, -7];
 // su forEach
 // atspausdinti visus skaicius
+nums.forEach((sk) => {
+  // console.log(sk);
+});
 // atspausdinti tik teigiamaus skaicius
-
+function printNums(num, idx, arr) {
+  if (num > 0) console.log('num ===', num);
+}
+nums.forEach(printNums);
 const mixed = ['red', true, 5, 7, 'white', 10];
 // su forEach
 // atspausdinti visus el tipus
 // atspausdinti skaiciu suma
+
+// nusitaikom i li el
+const liElList = document.querySelectorAll('#spalvos li');
+console.log('liElList ===', liElList);
+
+liElList.forEach((liEl) => {
+  console.log('liEl ===', liEl);
+  // liEl.append(' - spalva');
+  // liEl.textContent += ' - spalva';
+  // pakeisti fono splava i ta kkuri parasyta
+  liEl.style.backgroundColor = liEl.textContent;
+});
---
+++ js/practice.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log('practice.js file was loaded');
+
+const mixed = ['red', true, 5, 7, 'white', 10];
+
+// for i
+// for of
+// forEach
+
+// atspausdinti mixed masyvo tipus
+console.log(typeof mixed[0]);
+console.log(typeof mixed[1]);
