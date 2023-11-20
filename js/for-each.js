aa85f74b4439eda0bda0beca2a7a1f843d40f438
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-20 10:00:55 +0200

    uzd

---
+++ js/for-each.js
@@ -0,0 +1,22 @@
+'use strict';
+console.log('for-each.js file was loaded');
+
+const colors = ['red', 'green', 'blue', 'black', 'white'];
+
+for (let spalva of colors) {
+  console.log('spalva ===', spalva);
+}
+
+// arr.forEach((el, index, arr) => {function body})
+const colUpper = [];
+colors.forEach(function (spalva, idx, arr) {
+  console.log(`index: ${idx}`, spalva);
+  // galiu keisti esama masyva
+  // colors[idx] = spalva + ' spalva';
+  colUpper.push(spalva.toUpperCase());
+});
+console.log('colUpper ===', colUpper);
+console.log('colors ===', colors);
+
+const mixed = ['red', true, 5, 7, 'white', 10];
+const nums = [5, -7, 1, -8, 7, 15, -8, 2, -7];
