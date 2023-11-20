7c1cb9af157c3abc6696279c9309a8b65fdd00de
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-20 10:44:18 +0200

    uzd

--- js/practice.js
+++ js/practice.js
@@ -10,3 +10,12 @@ const mixed = ['red', true, 5, 7, 'white', 10];
 // atspausdinti mixed masyvo tipus
 console.log(typeof mixed[0]);
 console.log(typeof mixed[1]);
+
+// i nauja masyva sudeti tik skaicius
+
+// i nauja masyva sudeti tik stringus
+
+const nums = [1, 2, 3, 4];
+// sudeti masyvo suma
+
+// gauti masyvo vidurki
