46e39fd6d53d3e9547664abcb64375a0e5a95862
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-11-20 08:59:13 +0200

    for

--- js/array-loop-for.js
+++ js/array-loop-for.js
@@ -1,4 +1,39 @@
 'use strict';
 console.log('array-loop-for.js file was loaded');
 
-const colors = ['red', 'green', 'blue'];
+const colors = ['red', 'green', 'blue', 'black', 'white'];
+
+console.log('colors ===', colors);
+
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log('------');
+
+let colorsUpper = [];
+
+for (let i = 0; i < colors.length; i++) {
+  let element = colors[i];
+  let elUpper = element.toUpperCase();
+  // praleisti el su index 2
+  // if (i === 2) continue;
+  // if (element === 'black') break;
+  console.log(elUpper);
+  colorsUpper.push(elUpper);
+}
+console.log('colorsUpper ===', colorsUpper);
+
+// for of ========
+// for (let el of masyvas) {
+// kiekvienas masyvo el yra kintamasis su pavad el
+//}
+
+for (let element of colors) {
+  if (element === 'blue') continue;
+  // console.log('element ===', element);
+  console.log(`<li>${element}</li>`);
+}
+
+// su jusu pasirinktu budu
+// sugeneruoti li elemntus su document.createElement
+// sudeti juos i <ul id="spalvos"></ul>
